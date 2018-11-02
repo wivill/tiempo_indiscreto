ca38e89e85c53b2e35357cd31b0506493085805a
Willy <gopwivill@gmail.com>
2018-11-01 20:25:16 -0600

    convirtiendo simulink a R2017a

--- examen/matlab/Examen2.m
+++ examen/matlab/Examen2.m
@@ -1,6 +1,8 @@
 clc;
 clear all;
-%% Se definen par·metros
+%% Se definen par√°metros
+Ts = 0.1;
+Tf = 60;
 k = 9.22;
 T = 34.31;
 L = 3.17;
@@ -8,7 +10,10 @@ t = 0.15;
 P = 1/(k*(t+(L/T)));
 I = 1/(T);
 
-%% Se grafica Bode con m·rgenes de ganancia
+sim('PI1GDL', Tf);
+sim('PI1GDL_Z', Tf);
+
+%% Se grafica Bode con m√°rgenes de ganancia
 s = tf('s');
 C = P*(1+(I/s));
 P = k*(exp(-L*s))/(T*s+1);
