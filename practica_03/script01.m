ae09e7af07368e176875c952e847ff06a0446694
Willy <gopwivill@gmail.com>
2018-11-01 18:32:47 -0600

    PID 1GDL modificado para gráficos, IAE

--- practica_03/script01.m
+++ practica_03/script01.m
@@ -7,7 +7,7 @@ s = tf('s');
 z = tf('z', Ts);
 
 S1 = (3)/(s + 2);
-Z1 = c2d(S1, Ts);
+Z1 = c2d(S1, Ts, 'zoh');
 ssZ1 = ss(Z1);
 ssS1 = ss(S1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -17,7 +17,7 @@ C2 = [1 -2];
 D2 = 0;
 
 ssS2 = ss(A2, B2, C2, D2);
-ssZ2 = c2d(ssS2, Ts);
+ssZ2 = c2d(ssS2, Ts, 'zoh');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 A3 = [1 -4 5;-1 7 -1;1 3 4];
 B3 = [1;2;1];
@@ -25,12 +25,12 @@ C3 = [1 -1 1];
 D3 = 0;
 
 ssS3 = ss(A3, B3, C3, D3);
-ssZ3 = c2d(ssS3, Ts);
+ssZ3 = c2d(ssS3, Ts, 'zoh');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 S4 = (5)/(s + 25);
 
 ssS4 = ss(S4);
-ssZ4 = c2d(ssS4, Ts);
+ssZ4 = c2d(ssS4, Ts, 'zoh');
 
 % parte 2
 
@@ -39,4 +39,17 @@ sys_total = feedback(sys1,ssZ4);
 
 % parte 3
 
-ctrb(sys_total.A, sys_total.B); 
+Q = ctrb(sys_total);
+disp("Rango de matriz Q")
+rank(Q)
+disp("Determinante de matriz Q")
+det(Q)
+G = obsv(sys_total);
+disp("Rango de matriz G")
+rank(G)
+disp("Determinante de matriz G")
+det(G)
+
+% parte 4
+
+step(sys_total)
