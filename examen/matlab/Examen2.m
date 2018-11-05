efad2d5e3d0d02131b380a691c4df17c28851ae5
wivill <gopwivill@gmail.com>
2018-11-04 20:46:27 -0600

    diagramas en Z

--- examen/matlab/Examen2.m
+++ examen/matlab/Examen2.m
@@ -1,6 +1,11 @@
 clc;
 clear all;
-%% Se definen parámetros
+
+%% ParÃ¡metros generales de simulaciÃ³n
+Ts = 0.1;
+Tf = 200;
+
+%% Se definen parÃ¡metros del PI 1GDL
 k = 9.22;
 T = 34.31;
 L = 3.17;
@@ -8,11 +13,25 @@ t = 0.15;
 H = 1/(k*(t+(L/T)));
 I = 1/(T);
 
-%% Se grafica Bode con márgenes de ganancia
-s = tf('s');
-C = H*(1+(I/s));
-P = k*(exp(-L*s))/(T*s+1);
-L1 = C*P;
-figure;
-margin(L1);
-grid on;
+sim('PI1GDL', Tf);
+sim('PI1GDL_Z', Tf);
+
+%% Se definen parï¿½metros del controlador de 2GDL
+tc = 0.2;
+to = L/T;
+a = (tc*(2-tc))+to;
+Kp = (a)/(k*(tc+to).^2);
+Ti = T*(a/(1+to));
+B = (T*tc)/(Ti);
+
+sim('PI2GDL', Tf);
+sim('PI2GDL_Z', Tf);
+
+%% Se grafica Bode con mï¿½rgenes de ganancia
+% s = tf('s');
+% C = H*(1+(I/s));
+% P = k*(exp(-L*s))/(T*s+1);
+% L1 = C*P;
+% figure;
+% margin(L1);
+% grid on;
