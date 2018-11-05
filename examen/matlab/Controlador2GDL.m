159fbbce492b18230c19bf0ca7998531a4245238
RafaelAvendanoValverde <rafa_avendano@hotmail.com>
2018-11-04 23:32:00 -0600

    Add files via upload

--- examen/matlab/Controlador2GDL.m
+++ examen/matlab/Controlador2GDL.m
@@ -13,3 +13,14 @@ a = (tc*(2-tc))+to;
 Kp = (a)/(k*(tc+to).^2);
 Ti = T*(a/(1+to));
 B = (T*tc)/(Ti);
+
+%% Se grafica Bode con márgenes de ganancia
+s = tf('s');
+P = k*(exp(-L*s))/(T*s+1);
+Cr= Kp*(B+(1/s*Ti));
+Cy= Kp*(1+(1/s*Ti));
+C = Cr + Cy;
+L1 = C*P;
+figure;
+margin(L1);
+grid on;
