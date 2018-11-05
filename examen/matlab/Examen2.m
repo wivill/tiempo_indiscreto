1b37471f104db7a4bfe1d1a5327269910bd58d67
wivill <gopwivill@gmail.com>
2018-11-04 22:28:02 -0600

    fix a diagramas y gráficos

--- examen/matlab/Examen2.m
+++ examen/matlab/Examen2.m
@@ -3,7 +3,7 @@ clear all;
 
 %% Parámetros generales de simulación
 Ts = 0.1;
-Tf = 200;
+Tf = 150;
 
 %% Se definen parámetros del PI 1GDL
 k = 9.22;
@@ -19,10 +19,23 @@ sim('PI1GDL_Z', Tf);
 figure(1)
 grid on
 hold on
-title('Salida del sistema continuo y discreto')
-plot(Y1)
-plot(Y1_Z)
+plot(Y1, 'black')
+plot(Y1_Z, '--')
+plot(D1)
+plot(SP1)
 legend('Continuo', 'Discreto')
+title('Salida del sistema continuo y discreto, PI 1GDL')
+hold off
+
+figure(2)
+grid on
+hold on
+plot(U1, 'black')
+plot(U1_Z, '--')
+plot(D1)
+legend('Continuo', 'Discreto')
+title('Esfuerzo de control del sistema continuo y discreto, PI 1GDL')
+hold off
 
 %% Se definen par�metros del controlador de 2GDL
 tc = 0.2;
@@ -34,6 +47,28 @@ B = (T*tc)/(Ti);
 
 sim('PI2GDL', Tf);
 sim('PI2GDL_Z', Tf);
+
+figure(3)
+grid on
+hold on
+plot(Y2, 'black')
+plot(Y2_Z, '--')
+plot(D2)
+plot(SP2)
+legend('Continuo', 'Discreto')
+title('Salida del sistema continuo y discreto, PI 2GDL')
+hold off
+
+figure(4)
+grid on
+hold on
+plot(U2, 'black')
+plot(U2_Z, '--')
+plot(D2)
+legend('Continuo', 'Discreto')
+title('Esfuerzo de control del sistema continuo y discreto, PI 2GDL')
+hold off
+
 
 %% Se grafica Bode con m�rgenes de ganancia
 % s = tf('s');
