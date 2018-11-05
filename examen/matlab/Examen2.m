c008684f980291c07a5deaf24e158b4fd56f5ad6
wivill <gopwivill@gmail.com>
2018-11-05 01:46:58 -0600

    fragilidad

--- examen/matlab/Examen2.m
+++ examen/matlab/Examen2.m
@@ -118,35 +118,54 @@ bodeplot(Ms_SA,options)
 grid on
 title('Función de sensibilidad Ms con PI Sintonización Analítica')
 
+figure(7)
+fc_graf_polar_L(L_SA)
+grid on
+title('Diagrama de fase para Ms con PI Sintonización Analítica')
+
+
+
 % alfaro estricto
 C_est = Kp_es*(1+(I/s));
 L_est = C_est*P;
 Ms_est = 1/(1+L_est);
 [mGanancia_est,mFase_est,wmGanancia_est,wmFase_est] = margin(L_est);
-figure(7);
+figure(8);
 margin(L_est);
 title('Márgenes de fase y ganancia con PI Alfaro Control Estricto')
 grid on;
 
-figure(8)
+figure(9)
 bodeplot(Ms_est,options)
 grid on
 title('Función de sensibilidad Ms con PI Alfaro Control Estricto')
+
+figure(10)
+fc_graf_polar_L(L_est)
+grid on
+title('Diagrama de fase para Ms con PI Alfaro Control Estricto')
 
 % alfaro suave
 C_al = Kp_al*(1+(I/s));
 L_soft = C_al*P;
 Ms_soft = 1/(1+L_soft);
 [mGanancia_soft,mFase_soft,wmGanancia_soft,wmFase_soft] = margin(L_soft);
-figure(9);
+figure(11);
 margin(L_soft);
 title('Márgenes de fase y ganancia con PI Alfaro Control Suave')
 grid on;
 
-figure(10)
+figure(12)
 bodeplot(Ms_soft,options)
 grid on
 title('Función de sensibilidad Ms con PI Alfaro Control Suave')
+
+figure(13)
+fc_graf_polar_L(L_soft)
+grid on
+title('Diagrama de fase para Ms con PI Alfaro Control Suave')
+
+
 % 2GDL
 Cr= Kp*(B+(1/(s*Ti)));
 Cy= Kp*(1+(1/(s*Ti)));
@@ -154,12 +173,37 @@ L2 = (Cy+Cr)*P;
 Ms2 = 1/(1+L2);
 [mGanancia2,mFase2,wmGanancia2,wmFase2] = margin(L2);
 
-figure(11);
+figure(14);
 margin(L2);
 title('Márgenes de fase y ganancia con PI 2GDL')
 grid on;
 
-figure(12)
+figure(15)
 bodeplot(Ms2,options)
 grid on
 title('Función de sensibilidad Ms con PI 2GDL')
+
+figure(16)
+fc_graf_polar_L(L2)
+grid on
+title('Diagrama de fase para Ms con PI 2GDL')
+
+% figure(17)
+% fc_fragility_rings(Kp_es,Ti_es,0,P)
+% grid on
+% title('Fragilidad con PI Alfaro Control Estricto')
+% 
+% figure(18)
+% fc_fragility_rings(Kp_al,Ti_al,0,P)
+% grid on
+% title('Fragilidad con PI Alfaro Control Suave')
+% 
+% figure(19)
+% fc_fragility_rings(H,I,0,P)
+% grid on
+% title('Fragilidad con PI Sintonización Analítica')
+% 
+% figure(20)
+% fc_fragility_rings(Kp,Ti,0,P)
+% grid on
+% title('Fragilidad con PI 2GDL')
