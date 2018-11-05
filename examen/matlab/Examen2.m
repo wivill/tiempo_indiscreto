6981cfbabd734f751ae178d902d1f5bb520b6ae8
wivill <gopwivill@gmail.com>
2018-11-04 23:36:40 -0600

    margenes para controladores

--- examen/matlab/Examen2.m
+++ examen/matlab/Examen2.m
@@ -1,9 +1,12 @@
 clc;
-clear all;
+clear;
 
 %% Parámetros generales de simulación
 Ts = 0.1;
 Tf = 150;
+s = tf('s');
+z = tf('z', Ts);
+options = bodeoptions; options.MagUnits = 'abs';
 
 %% Se definen parámetros del PI 1GDL
 k = 9.22;
@@ -23,7 +26,9 @@ plot(Y1, 'black')
 plot(Y1_Z, '--')
 plot(D1)
 plot(SP1)
-legend('Continuo', 'Discreto')
+ylabel('Amplitud')
+xlabel('Tiempo T/(s)')
+legend('Continuo', 'Discreto', 'Perturbación', 'Referencia')
 title('Salida del sistema continuo y discreto, PI 1GDL')
 hold off
 
@@ -33,7 +38,9 @@ hold on
 plot(U1, 'black')
 plot(U1_Z, '--')
 plot(D1)
-legend('Continuo', 'Discreto')
+ylabel('Amplitud')
+xlabel('Tiempo T/(s)')
+legend('Continuo', 'Discreto', 'Perturbación')
 title('Esfuerzo de control del sistema continuo y discreto, PI 1GDL')
 hold off
 
@@ -55,7 +62,9 @@ plot(Y2, 'black')
 plot(Y2_Z, '--')
 plot(D2)
 plot(SP2)
-legend('Continuo', 'Discreto')
+ylabel('Amplitud')
+xlabel('Tiempo T/(s)')
+legend('Continuo', 'Discreto', 'Perturbación', 'Referencia')
 title('Salida del sistema continuo y discreto, PI 2GDL')
 hold off
 
@@ -65,16 +74,42 @@ hold on
 plot(U2, 'black')
 plot(U2_Z, '--')
 plot(D2)
-legend('Continuo', 'Discreto')
+ylabel('Amplitud')
+xlabel('Tiempo T/(s)')
+legend('Continuo', 'Discreto', 'Perturbación')
 title('Esfuerzo de control del sistema continuo y discreto, PI 2GDL')
 hold off
 
-
 %% Se grafica Bode con m�rgenes de ganancia
-% s = tf('s');
-% C = H*(1+(I/s));
-% P = k*(exp(-L*s))/(T*s+1);
-% L1 = C*P;
-% figure;
-% margin(L1);
-% grid on;
+% PI 1GDL
+C = H*(1+(I/s));
+P = k*(exp(-L*s))/(T*s+1);
+L1 = C*P;
+Ms1 = 1/(1+L1);
+[mGanancia1,mFase1,wmGanancia1,wmFase1] = margin(L1);
+figure(5);
+margin(L1);
+title('Márgenes de fase y ganancia para el sistema con PI 1GDL')
+grid on;
+
+figure(6)
+bodeplot(Ms1,options)
+grid on
+title('Medición de función de sensibilidad Ms para sistema con PI 1GDL')
+
+% 2GDL
+Cr= Kp*(B+(1/(s*Ti)));
+Cy= Kp*(1+(1/(s*Ti)));
+L2 = (Cy+Cr)*P;
+Ms2 = 1/(1+L2);
+[mGanancia2,mFase2,wmGanancia2,wmFase2] = margin(L2);
+
+figure(7);
+margin(L2);
+title('Márgenes de fase y ganancia para el sistema con PI 2GDL')
+grid on;
+
+figure(8)
+bodeplot(Ms2,options)
+grid on
+title('Medición de función de sensibilidad Ms para sistema con PI 2GDL')
